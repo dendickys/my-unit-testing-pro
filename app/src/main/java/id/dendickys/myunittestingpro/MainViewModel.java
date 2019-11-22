cfb6fdb2afe4e018802627ac4365e101d3fb0b25
dendickys <febri.unquad2014@gmail.com>
2019-11-22 20:40:09 +0700

    example unit testing

---
+++ app/src/main/java/id/dendickys/myunittestingpro/MainViewModel.java
@@ -0,0 +1,21 @@
+package id.dendickys.myunittestingpro;
+
+class MainViewModel {
+    private CuboidModel cuboidModel;
+    MainViewModel(CuboidModel cuboidModel) {
+        this.cuboidModel = cuboidModel;
+    }
+    void save(double l, double w, double h) {
+        cuboidModel.save(l, w, h);
+    }
+    double getCircumference() {
+        return cuboidModel.getCircumference();
+    }
+    double getSurfaceArea() {
+        return cuboidModel.getSurfaceArea();
+    }
+    double getVolume() {
+        return cuboidModel.getVolume();
+    }
+
+}
